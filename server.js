2a40693667127b21cf3ded051de216701e60e526
Shahzaibkhan98 <45264436+Shahzaibkhan98@users.noreply.github.com>
2020-05-08 23:55:16 +0500

    First Commit

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+// const users = require('./routes/api/users');
+// const profile = require('./routes/api/profile');
+// const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -14,6 +29,10 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 3000;
+// app.use('/api/users', users);
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
